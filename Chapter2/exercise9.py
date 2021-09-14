0b3fc7315eb9cd21531889caffd1fd394568ae40
Hoang Chu <kamikiburning2002@gmail.com>
2021-09-14 14:39:30 -0500

    Fix exerciese9

--- Chapter2/exercise9.py
+++ Chapter2/exercise9.py
@@ -1,5 +1,5 @@
 print("*** Celsius to Fahrenheit Temperature Converter ***")
 
-temperature = float(input("Enter the Celsius degree: "))
+temperature = float(input("Give me a temperature in Celsius: "))
 fahrenheit = 9/5 * temperature + 32
 print("F = " + str(int(fahrenheit)))
--- Chapter2/input.py
+++ Chapter2/input.py
@@ -1,6 +1,7 @@
-print("What is your is your name?")
-name = input()
-print("Nice to meet you, " + name + "!")
+# print("What is your is your name?")
+# name = input()
+# print("Nice to meet you, " + name + "!")
 
 name = input("What is you name? \n")
 print("Hi again, " + name)
+
